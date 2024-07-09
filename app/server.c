2eed047b3e9424b2f9fed0619986f79f001ddfb0
0ussamabernou <oussama.bernou@outlook.com>
2024-07-09 13:16:23 +0100

    respond 200

--- app/server.c
+++ app/server.c
@@ -56,8 +56,11 @@ int main() {
   printf("Waiting for a client to connect...\n");
   client_addr_len = sizeof(client_addr);
 
-  accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+  int fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
   printf("Client connected\n");
+
+  char *reply = "HTTP/1.1 200 OK\r\n\r\n";
+  int bytes_sent = send(fd, reply, strlen(reply), 0);
 
   close(server_fd);
 
