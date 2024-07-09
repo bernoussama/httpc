7fd6ae0f5afaa8e588d62247a4f50671cf1f2ae0
0ussamabernou <oussama.bernou@outlook.com>
2024-07-09 17:06:03 +0100

    extract url path

--- app/server.c
+++ app/server.c
@@ -7,6 +7,16 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+typedef struct http_request {
+  char *method;
+  char *path;
+  char *version;
+  char **headers; // Maximum 10 headers
+  char *body;
+} http_request;
+
+http_request *parse_request(char *request);
+
 int main() {
   // Disable output buffering
   setbuf(stdout, NULL);
@@ -20,6 +30,7 @@ int main() {
   //
   int server_fd, client_addr_len;
   struct sockaddr_in client_addr;
+  char request_buffer[1024];
 
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd == -1) {
@@ -59,10 +70,74 @@ int main() {
   int fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
   printf("Client connected\n");
 
-  char *reply = "HTTP/1.1 200 OK\r\n\r\n";
-  int bytes_sent = send(fd, reply, strlen(reply), 0);
+  // Receive the HTTP request
+  int bytes_received = recv(fd, request_buffer, sizeof(request_buffer) - 1, 0);
+  if (bytes_received == -1) {
+    perror("recv failed");
+    exit(1);
+  }
+  request_buffer[bytes_received] = '\0';
+  char *request_string = request_buffer;
+  http_request *request = parse_request(request_string);
+  if (request == NULL) {
+    printf("Failed to parse request\n");
+    return 1;
+  }
+
+  char *response;
+  // if path is "/"
+  if (strcmp(request->path, "/") == 0) {
+    response = "HTTP/1.1 200 OK\r\n\r\n";
+  } else {
+    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+  }
+
+  // server response
+  int bytes_sent = send(fd, response, strlen(response), 0);
 
   close(server_fd);
 
   return 0;
+}
+
+http_request *parse_request(char *request) {
+  http_request *req = malloc(sizeof(http_request));
+  if (request == NULL) {
+    return (NULL); // Error handling: memory allocation failed
+  }
+
+  // parse method
+  char *method = strtok(request, " ");
+  if (method == NULL) {
+    // Error handling: invalid request format
+    free(req);
+    return (NULL);
+  }
+  req->method = malloc(strlen(method) + 1);
+  strcpy(req->method, method);
+
+  // parse path
+  char *path = strtok(NULL, " ");
+  if (path == NULL) {
+    // Error handling: invalid request format
+    free(req->method);
+    free(req);
+    return (NULL);
+  }
+  req->path = malloc(strlen(path) + 1);
+  strcpy(req->path, path);
+
+  // parse version
+  char *version = strtok(NULL, "\r\n");
+  if (version == NULL) {
+    // Error handling: invalid request format
+    free(req->method);
+    free(req->path);
+    free(req);
+    return (NULL);
+  }
+  req->version = malloc(strlen(version) + 1);
+  strcpy(req->version, version);
+
+  return req;
 }
